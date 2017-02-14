1bf2c74c1e53975fb1a329e017ff3d354b111e2d
Andrew Ledvina <wvvwwvw@gmail.com>
2017-02-14 11:18:41 -0800

    Add rollbar and get webpack working without json-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = [{
+  name: 'frontend',
   devtool: 'eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'app/main.js')
-  ],
+  entry: path.join(__dirname, 'app/main.js'),
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name].js',
@@ -22,7 +21,6 @@ module.exports = {
       filename: 'index.html'
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
@@ -38,4 +36,29 @@ module.exports = {
       }
     }]
   }
-};
+}, {
+  name: 'server',
+  target: 'node',
+  node: {
+    __filename: false,
+    __dirname: false
+  },
+  externals: [ nodeExternals() ],
+  entry: path.join(__dirname, 'server.js'),
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'server.js'
+  },
+  plugins: [
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        "presets": ["react", "es2015", "stage-0"]
+      }
+    }]
+  }
+}];
